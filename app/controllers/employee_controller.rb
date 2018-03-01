7a602721bc7743b773ffb2bb7b2b02d63ca6fee2
stefan <hotpea@gmail.com>
2018-03-01 00:53:53 -0300

    validations

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -21,9 +21,23 @@ class EmployeeController < ApplicationController
     @employee.birth = params[:birth]
     @employee.salary = params[:salary]
     @employee.department_id = params[:department_id]
-    @employee.save
 
-    redirect_to '/employee/index'
+    manager = Department.exists?(employee_id: params[:id])
+
+    # manager = false
+
+    if Time.now.year - 18 < @employee.birth.year
+      redirect_to request.referrer, flash: {error: 'Funcionário precisa ter mais de 18 anos!!!'}
+    elsif (params[:salary].to_f > 10000) && (not manager)
+      redirect_to request.referrer, flash: {
+          error: 'Funcionário não pode ganhar mais de R$10.000 se não for gerente de departamento!!!',
+          msg: 'Vá em editar departamento e adicione este usuário a um departamento como gerente e volte aqui e tente aumentar seu salário'
+      }
+    else
+      @employee.save
+
+      redirect_to '/employee/index'
+    end
   end
 
   def edit
